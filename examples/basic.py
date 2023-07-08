2fc511293948ceaa24e82f5399171910e6be18f4
manatlan <manatlan@gmail.com>
2023-07-08 14:29:56 +0200

    it takes form ;-)

--- examples/basic.py
+++ examples/basic.py
@@ -13,86 +13,76 @@ async def homepage(request):
 
 async def inc(request):
     global DATA
-    Manager.send("ping","hllllll")
+    Manager().ping( "hello" )
     DATA+=1
     return RedirectResponse("/")
 
-p=multiprocessing.Manager().dict()
+
+
+def mainprocess(input,output):
+    print("MAINPROCESS")
+
+    async def ping(msg):
+        return f"hello {msg}"
+
+    methods=locals()
+
+    async def loop():
+        while 1:
+            action,(a,k) = input.recv()
+            print("::: RECV=",action,file=sys.stdout,flush=True)
+            if action=="quit":
+                break
+
+            method=methods[action]
+            # logger.info("Process %s: %s",uid,action)
+            r=await method(*a,**k)
+
+            output.send( r )
+
+    asyncio.run( loop() )
+    print("MAINPROCESS EXITED")
+
 
 class Manager:
-    @staticmethod
-    def start():
+    _p=multiprocessing.Manager().dict()
 
-        def mainprocess(input,output):
-            print("MAINPROCESS")
-
-            async def ping(msg):
-                return f"hello {msg}"
-
-            methods=locals()
-
-            async def loop():
-                while 1:
-                    action,(a,k) = input.recv()
-                    print("::: RECV=",action,file=sys.stdout,flush=True)
-                    if action=="quit":
-                        break
-
-                    method=methods[action]
-                    # logger.info("Process %s: %s",uid,action)
-                    r=await method(*a,**k)
-
-                    output.send( r )
-
-            asyncio.run( loop() )
-            print("MAINPROCESS EXITED")
-
-        if not p:
+    def __init__(self):
+        if not Manager._p:
             qs,qr=multiprocessing.Pipe()
             rs,rr=multiprocessing.Pipe()
-            p["input"]=qs
-            p["output"]=rr
+            Manager._p["input"]=qs
+            Manager._p["output"]=rr
 
             ps = multiprocessing.Process(target=mainprocess, args=[qr,rs])
             ps.start()
-            return ps
-        else:
-            return None
 
-    @staticmethod
-    def quit():
-        p["input"].send( ("quit",([],{}) ))
+        self.pp=Manager._p
 
-    @staticmethod
-    def send(method,*a,**k):
-        p["input"].send( (method,(a,k)))
+    def shutdown(self):
+        if self.pp:
+            self.pp["input"].send( ("quit",([],{}) ))
+            self.pp=None
+            del Manager._p
 
-        x=p["output"].recv()
-        print(f"::manager action {method}({a}), recept={x}")
-        return x
+    def __getattr__(self,action:str):
+        def _(*a,**k):
+            self.pp["input"].send( (action,(a,k)))
 
-    # @classmethod
-    # def __getattr__(cls,action:str):
-    #     async def _(*a,**k):
-    #         p["input"].send( (action,(a,k)))
+            x=self.pp["output"].recv()
+            print(f"::manager action {action}({a}), recept={x}")
+            return x
+        return _
 
-    #         x=p["output"].recv()
-    #         return x
-    #     return _
-
-ps=None
 
 async def startup():
-    global ps
-    ps=Manager.start()
+    c=Manager()     # only one will run !
 
     await asyncio.sleep(1)
-    Manager.send( "ping","debut" )
+    c.ping( "debut" )
 
 async def shutdown():
-    global ps
-    if ps: # the one who have started it, kill it ;-)
-        Manager.quit()
+    Manager().shutdown()
 
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
