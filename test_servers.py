79005a13f95bcef3f2f83defce9c0de7acec20bc
manatlan <manatlan@gmail.com>
2023-07-10 08:25:22 +0200

    all UT at 90%

--- test_servers.py
+++ test_servers.py
@@ -2,15 +2,19 @@ import pytest
 from htag import Tag
 from htagweb import findfqn,WebServer,WebServerWS
 import htagweb
-import sys
+import sys,json
 from starlette.testclient import TestClient
 
 class MyTag(Tag.div): pass
-class App(Tag.div): pass
-
-class Hello(Tag.div):
+class App(Tag.body):
     def init(self,p="nobody"):
-        self.set(f"hello {p}")
+        self.b=Tag.Button("say hello",_onclick=self.bind.doit() )
+
+        self+=(f"hello {p}")
+        self+=self.b
+    def doit(self):
+        self+="hello"
+
 
 class Nimp: pass
 
@@ -24,60 +28,70 @@ def test_fqn():
     with pytest.raises( Exception ):
         findfqn(Nimp)
 
+    with pytest.raises( Exception ):
+        findfqn(sys)
+
     assert findfqn("mod.name") == "mod.name"
     assert findfqn(MyTag) in ["__main__.MyTag","test_servers.MyTag"]
     assert findfqn(sys.modules[__name__]) in ["__main__.App","test_servers.App"]
 
-def test_app_served():
+@pytest.fixture( params=["wh_solo","wh_served","ws_solo","ws_served"] )
+def app(request):
+    if request.param=="wh_solo":
+        return WebServer( App )
+
+    elif request.param=="ws_solo":
+        return WebServerWS( App )
+
+    elif request.param=="wh_served":
+        app=WebServer()
+        async def handlePath(request):
+            return await request.app.serve(request, App)
+        app.add_route("/", handlePath )
+        return app
+
+    elif request.param=="ws_served":
+        app=WebServerWS()
+        async def handlePath(request):
+            return await request.app.serve(request, App)
+        app.add_route("/", handlePath )
+        return app
+
+def test_app_webserver_basic(app):
 
     htagweb.MANAGER = htagweb.Manager()
 
     try:
-        for HTServer in [WebServer,WebServerWS]:
-            app=HTServer( Hello )
-            client = TestClient(app)
+        client=TestClient(app)
 
-            response = client.get('/')
-            assert response.status_code == 200
-            assert "hello nobody" in response.text
+        response = client.get('/')
+        assert response.status_code == 200
+        assert "hello nobody" in response.text
 
-            response = client.get('/?p=world')
-            assert response.status_code == 200
-            assert "hello world" in response.text
+        response = client.get('/?p=world')
+        assert response.status_code == 200
+        assert "hello world" in response.text
 
-            response = client.get('/?kiki')
+        response = client.get('/?kiki')
+        assert response.status_code == 200
+        assert "hello kiki" in response.text
+
+        fqn=findfqn( App )
+        msg=dict(id="ut",method="doit",args=(),kargs={})
+        if isinstance( app, WebServer):
+            response = client.post("/"+fqn,json=msg)
             assert response.status_code == 200
-            assert "hello kiki" in response.text
+            assert "update" in response.json()
+        else:
+            with client.websocket_connect('/ws?fqn='+fqn) as websocket:
+                websocket.send_text( json.dumps(msg) )
+                data = websocket.receive_text()
+                assert "update" in json.loads(data)
     finally:
         htagweb.MANAGER.shutdown()
 
-def test_app_with_serve():
-    htagweb.MANAGER = htagweb.Manager()
-
-    try:
-        for HTServer in [WebServer,WebServerWS]:
-            app=HTServer()
-            async def handlePath(request):
-                return await request.app.serve(request, Hello)
-            app.add_route("/", handlePath )
-
-            client = TestClient(app)
-
-            response = client.get('/')
-            assert response.status_code == 200
-            assert "hello nobody" in response.text
-
-            response = client.get('/?p=world')
-            assert response.status_code == 200
-            assert "hello world" in response.text
-
-            response = client.get('/?kiki')
-            assert response.status_code == 200
-            assert "hello kiki" in response.text
-    finally:
-        htagweb.MANAGER.shutdown()
 
 if __name__=="__main__":
     # test_fqn()
-    test_app_served()
-    # test_app_with_serve()
+    # test_app_webserver_basic()
+    pass
