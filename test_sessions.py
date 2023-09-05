bc7a821aead025acd831826d97570ef237abc65c
manatlan <manatlan@gmail.com>
2023-09-05 18:13:59 +0200

    tu ok (but no mem session)

--- test_sessions.py
+++ test_sessions.py
@@ -13,15 +13,15 @@ def test_sessions_basics():
         finally:
             state.clear()
 
-def test_sessions_memory():
-    async def test():
-        state = createMem("uid")
-        try:
-            nb=state.get("nb",0)
-            nb+=1
-            state["nb"]=nb
-            assert state["nb"]==1
-        finally:
-            state.clear()
+# def test_sessions_memory():
+#     async def test():
+#         state = createMem("uid")
+#         try:
+#             nb=state.get("nb",0)
+#             nb+=1
+#             state["nb"]=nb
+#             assert state["nb"]==1
+#         finally:
+#             state.clear()
 
-    asyncio.run(test())
+#     asyncio.run(test())
